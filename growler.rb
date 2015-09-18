631c32a2306ac0f0eb797eaf15bd25ada4225df3
Charles Bittner-Rossmiller <charles.bittnerrossmiller@gmail.com>
2015-09-18 11:16:51 -0500

    Working pages but input wont print on homepage

--- growler.rb
+++ growler.rb
@@ -1,21 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-before do
-
-end
-
 enable :sessions
 
-#Routes
+before do
+	session[:growlHistory] ||= []
+end
+
 get '/' do
 	@name = session[:name]
-	# "Hello, #{session[:name]}"
 	erb :homepage
 end
 
-get '/newGrowl' do	
-	@name = session[:name]
-
+get '/newGrowl' do
 	erb :newGrowl
+end
+
+# Add a new growl
+post '/newGrowl' do
+	session[:growlHistory] << params[:newGrowl]
+	@newGrowl = params[:newGrowl]
+	erb :homepage
+end
+
+get '/homepage' do
+	@growlHistory = session[:growlHistory]
+	erb :homepage
+end
+
+post '/homepage' do
+	puts :growlHistory
 end
